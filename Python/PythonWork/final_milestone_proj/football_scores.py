a5743286cae49d92293b8d8be3d36b3ed4d91ea9
Sean McClannahan <sean1@iastate.edu>
2023-01-09 12:07:05 -0600

    added basic html website

--- Python/PythonWork/final_milestone_proj/football_scores.py
+++ Python/PythonWork/final_milestone_proj/football_scores.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import csv
 
 driver = webdriver.Firefox()
 
 driver.get("https://www.livesport.com/en/nfl/#/f3Ed5KM2/table/overall")
 
 games = []
+scores = open('scores.csv', 'w')
+csv_scores = csv.writer(scores)
+csv_scores.writerow(['Home Team', 'Away Team', 'Home Score', 'Away Score'])
 
 homeTeam = driver.find_elements(By.CLASS_NAME, "event__participant--home")
 awayTeam = driver.find_elements(By.CLASS_NAME, "event__participant--away")
@@ -14,8 +18,11 @@ awayScore = driver.find_elements(By.CLASS_NAME, "event__score--away")
 
 
 for game in range(0, len(homeTeam)):
-    games.append((homeTeam[game].text,awayTeam[game].text,homeScore[game].text,awayScore[game].text))
-    
-print(games[0])
+    if(homeScore[game].text == '-' or awayScore[game].text == '-'):
+        continue
+    else:
+        games.append((homeTeam[game].text,awayTeam[game].text,homeScore[game].text,awayScore[game].text))
+        csv_scores.writerow([homeTeam[game].text,awayTeam[game].text,homeScore[game].text,awayScore[game].text])
 
 driver.close()
+scores.close()
